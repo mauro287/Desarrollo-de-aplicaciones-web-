76ae86ecd092cfaa1fe2f211fb9dca1efd269c82
mauro287 <mr.naulaq@uea.edu.ec>
2026-02-20 17:11:26 -0500

    semana10

---
+++ proyecto/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Renderiza la plantilla index.html
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    # Renderiza la plantilla about.html
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
